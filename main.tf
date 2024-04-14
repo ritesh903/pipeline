069a0fbbc4b4f47fb94c6a130dba95bd4f58470d
rt <ritesh@example.com>
2024-04-14 22:50:26 +0530

    tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  region = "us-east-1"  # Change this to your desired region
+  region = "us-east-1" # Change this to your desired region
 }
 
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "example-bucket-name12321256443"  # Change this to your desired bucket name
+  bucket = "example-bucket-name12321256443" # Change this to your desired bucket name
   acl    = "private"
 }
